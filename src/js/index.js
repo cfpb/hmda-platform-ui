5bc53c3c30f3b58f6f00314480df5d944bec692f
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-03-11 01:33:07 -0800

    revert index.jsx

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var api = require('./api');
-var fiContainer = require('./fiContainer');
+var fiContainer = require('./fiContainer.jsx');
 //var makeSubmitForm = require('./makeSubmitForm.jsx');
 
 /*
@@ -11,8 +11,9 @@ var SubmitForm = makeSubmitForm('/submit', function(e){
 */
 
 api.getInstitutions(function(institutions){
+  console.log(institutions);
   ReactDOM.render(
-    <fiContainer institutions={institutions}/>,
+    React.createElement(fiContainer, null, {institutions: institutions}),
     document.getElementById('app')
   );
 });
