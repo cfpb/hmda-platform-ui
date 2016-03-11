7dcdce847f91b7322fe8daae62a54eb76a903193
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-03-10 19:58:32 -0800

    api front-end mock

---
+++ src/js/api.js
@@ -0,0 +1,96 @@
+module.exports = {
+  getInstitutions: function(){
+    return {
+      "institutions": [
+        {
+          "name": "Bank 0",
+          "status": 0,
+          "editReports": []
+        },
+        {
+          "name": "Bank 1",
+          "status": 1,
+          "editReports": []
+        },
+        {
+          "name": "Bank 1",
+          "status": 2,
+          "editReports": [
+          {
+              "timestamp": 1457484448191,
+              "edits": {
+                "syntactical": 2,
+                "validity": 1,
+                "quality": 2,
+                "macro": 1
+              }
+            }
+          ]
+        },
+        {
+          "name": "Bank 3",
+          "status": 3,
+          "editReports": [
+            {
+              "timestamp": 1457494448191,
+              "edits": {
+                "syntactical": 0,
+                "validity": 0,
+                "quality": 1,
+                "macro": 0
+              }
+            },
+            {
+              "timestamp": 1457484448191,
+              "edits": {
+                "syntactical": 2,
+                "validity": 1,
+                "quality": 2,
+                "macro": 1
+              }
+            },
+            {
+              "timestamp": 1457464448191,
+              "edits": {
+                "syntactical": 4,
+                "validity": 3,
+                "quality": 2,
+                "macro": 1
+              }
+            }
+          ]
+        },
+        {
+          "name": "Bank 4",
+          "status": 4,
+          "editReports": [
+            {
+              "timestamp": 1457494448191,
+              "edits": {
+                "syntactical": 0,
+                "validity": 0,
+                "quality": 2,
+                "macro": 0
+              }
+            }
+          ]
+        },
+        {
+          "name": "Bank 5",
+          "status": 5,
+          "editReports": [
+            {
+              "timestamp": 1457494448191,
+              "edits": {
+                "syntactical": 0,
+                "validity": 0,
+                "quality": 1,
+                "macro": 0
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+};
