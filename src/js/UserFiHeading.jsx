60cd8d88d4e73f357e02f150441c6f1d91130c87
Andrew <awolfe76@gmail.com>
2016-03-22 16:06:04 -0400

    set heading text immediately and rewrite if needed

--- src/js/UserFiHeading.jsx
+++ src/js/UserFiHeading.jsx
@@ -1,12 +1,10 @@
 var React = require('react');
 var UserFiHeading = React.createClass({
   render: function(){
-    var headingText = null;
+    var headingText = 'Welcome to ' + this.props.year + ' HMDA filing, ' + this.props.user;
 
     if (this.props.institution.name) {
       headingText = this.props.user + ' filing in ' + this.props.year + ' on behalf of ' + this.props.institution.name;
-    } else {
-      headingText = 'Welcome to ' + this.props.year + ' HMDA filing, ' + this.props.user;
     }
 
     return <h1>{headingText}</h1>
