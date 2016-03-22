2aaf4121e4fe92d82d516322a51ed6a01c0fc54a
Andrew <awolfe76@gmail.com>
2016-03-22 09:27:46 -0400

    use jsx in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,20 +6,19 @@ var FiContainer = require('./FiContainer.jsx');
 
 
 ReactDOM.render(
-  React.createElement(UserSelect, {callback: selectCallback}),
+  <UserSelect callback={selectCallback} />,
   document.getElementById('userSelectRoot')
 );
 
 var container = ReactDOM.render(
-                  React.createElement(FiContainer, {institutions: []}),
-                  document.getElementById('app')
-                );
+  <FiContainer institutions={[]} />,
+  document.getElementById('app')
+);
 
 function selectCallback(e){
   var user = e.target.value;
   console.log('selecting user: ', user);
   api.getInstitutions(user, function(institutions){
-   container.updateInstitutions(institutions);
+    container.updateInstitutions(institutions);
   });
 }
-
