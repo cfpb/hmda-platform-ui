525450d9693959b58d95f33013337db24ca46bcd
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-04-01 01:21:49 -0700

    homelink component

---
+++ src/js/HomeLink.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var Link = require('react-router').Link;
+
+var HomeLink = React.createClass({
+  render: function(){
+    return <Link className="HomeLink" to="/">Home</Link>
+  }
+});
+
+module.exports = HomeLink;
