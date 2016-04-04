997ce9d22525ac8e9b0a1a029cb70e23583bc3e3
Andrew <awolfe76@gmail.com>
2016-04-04 16:29:12 -0400

    cleaining up a bit

--- src/js/ErrorsContainer.jsx
+++ src/js/ErrorsContainer.jsx
@@ -56,6 +56,45 @@ var ErrorsContainer = React.createClass({
               	}
               ]
             }
+          ],
+          valid: [
+            {
+              "loanNumber": "123455",
+            	"errors": [
+                {
+              		"id": 1,
+              		"desc": "Here is a desc",
+              		"field": "Year",
+              		"valueSubmitted": "1967"
+              	}, {
+              		"id": 2,
+              		"desc": "Here is another desc",
+              		"field": "Year",
+              		"valueSubmitted": "1800"
+              	}, {
+              		"id": 3,
+              		"desc": "Here is another desc",
+              		"field": "Year",
+              		"valueSubmitted": "1800"
+              	}
+              ]
+            },
+            {
+              "loanNumber": "09876",
+            	"errors": [
+                {
+              		"id": 1,
+              		"desc": "Here is a desc",
+              		"field": "Year",
+              		"valueSubmitted": "1967"
+              	}, {
+              		"id": 2,
+              		"desc": "Here is another desc",
+              		"field": "Year",
+              		"valueSubmitted": "1800"
+              	}
+              ]
+            }
           ]
         }
       }
@@ -69,6 +108,8 @@ var ErrorsContainer = React.createClass({
           <p>Filing progress will go here. It could be in progress or complete or ...</p>
         </div>
         <div className="two-third">
+          <h2>Syntactical Errors</h2>
+          <p>This is a description of the syntactical errors. It will provide information to filers about the errors they are about to see.</p>
           <ErrorsSyntax errors={this.state.errors.syntax} />
         </div>
       </div>
--- src/js/ErrorsSyntax.jsx
+++ src/js/ErrorsSyntax.jsx
@@ -4,40 +4,6 @@ var ErrorSyntax = React.createClass({
   propTypes: {
     errors: React.PropTypes.array
   },
-  createErrorRow: function(loan) {
-    return (
-      <tr key={loan.loanNumber}>
-        <td>{loan.loanNumber}</td>
-        <td>{loan.errors.length}</td>
-      </tr>
-    )
-  },
-  createErrorDetailRow: function(error) {
-    return (
-      <tr key={error.id}>
-        <td>
-          <table>
-            <thead>
-              <tr>
-                <th>Edit ID</th>
-                <th>Description</th>
-                <th>Field</th>
-                <th>Submitted Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{error.id}</td>
-                <td>{error.desc}</td>
-                <td>{error.field}</td>
-                <td>{error.valueSubmitted}</td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      </tr>
-    )
-  },
   render: function() {
     console.log(this.props.errors);
     var _this = this;
@@ -47,31 +13,33 @@ var ErrorSyntax = React.createClass({
         <div className="tableHeader half">Errors</div>
         {this.props.errors.map(function(loan, i) {
           return (
-            <div>
-              <div className="half">{loan.loanNumber}</div>
-              <div className="half">{loan.errors.length}</div>
-              <table width="100%">
-                <thead>
-                  <tr>
-                    <th>Edit ID</th>
-                    <th>Description</th>
-                    <th>Field</th>
-                    <th>Submitted Value</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {loan.errors.map(function(error, i) {
-                    return (
-                      <tr key={error.id}>
-                        <td>{error.id}</td>
-                        <td>{error.desc}</td>
-                        <td>{error.field}</td>
-                        <td>{error.valueSubmitted}</td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
+            <div className="ErrorsSummary">
+              <div className="half summary">{loan.loanNumber}</div>
+              <div className="half summary">{loan.errors.length}</div>
+              <div className="ErrorsDetails">
+                <table width="100%">
+                  <thead>
+                    <tr>
+                      <th>Edit ID</th>
+                      <th>Description</th>
+                      <th>Field</th>
+                      <th>Submitted Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {loan.errors.map(function(error, i) {
+                      return (
+                        <tr key={error.id}>
+                          <td>{error.id}</td>
+                          <td>{error.desc}</td>
+                          <td>{error.field}</td>
+                          <td>{error.valueSubmitted}</td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </div>
           )
         })}
