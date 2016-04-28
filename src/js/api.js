9ffb2c6c6c7f0d6e02747af8bbc739cdd97774db
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-04-28 14:28:20 -0700

    remove cruft

--- src/js/api.js
+++
@@ -1,9 +0,0 @@
-var data = require('./data/institutions.js');
-
-module.exports = {
-  getInstitutions: function(name, cb){
-    return cb(data[name] || []);
-  },
-  getErrors: function(){console.log('getting errors')},
-  getProgress: function(){console.log('getting progress')}
-};
