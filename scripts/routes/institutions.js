59a998249b94fcc7e0ed11e781e0a5cfa9aabfdd
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-05-02 00:56:20 -0700

    institutions and years routes

--- scripts/routes/institutions.js
+++ scripts/routes/institutions.js
@@ -9,7 +9,7 @@ router.get('/:institution', function(req, res){
   res.send({name: 'some institution', id: req.params.institution})
 });
 
-router.use('/year', yearRouter);
+router.use('/:institution/years', yearRouter);
 
 
 module.exports = router;
--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-var util = require('util');
 var express = require('express');
 var historyApiFallback = require('connect-history-api-fallback');
 var apiRouter = require('./routes/api');
@@ -11,13 +10,6 @@ app.use(express.static('dist'));
 
 //serve the api, routes are nested in various modules
 app.use('/api', apiRouter);
-
-app.post('/api/institutions/:institution/years/:year', function (req, res) {
-  res.status(202).send({
-    id: 1,
-    progress: util.format('/api/institutions/%s/years/%s/submissions/1', req.params.institution, req.params.year)
-  });
-});
 
 //serve the app even when a user refreshes from a client-side route defined by the history api
 app.use(historyApiFallback());
