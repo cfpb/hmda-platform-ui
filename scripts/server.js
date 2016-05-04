b8a891c0fdd542d51e4ea0e2f7aed659868bac95
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-05-03 17:10:30 -0700

    Update history fallback solution

--- scripts/server.js
+++ scripts/server.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
 
 var express = require('express');
-var historyApiFallback = require('connect-history-api-fallback');
+var historyApiFallback = require('express-history-api-fallback');
 var apiRouter = require('./routes/api');
 
 var app = express();
 //serve the app
+app.use(function(req, res, next){
+  console.log(req.url);
+  next();
+})
+
 app.use(express.static('dist'));
 
 //serve the api, routes are nested in various modules
 app.use('/api', apiRouter);
 
 //serve the app even when a user refreshes from a client-side route defined by the history api
-app.use(historyApiFallback());
+app.use(historyApiFallback('dist/index.html', {root: '.'}));
 
 app.listen(3000);
