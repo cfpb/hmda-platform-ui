824b9cd1af2ab1642b495b16853bac9b88467cf6
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-05-07 11:04:19 -0700

    Redo status links to be in line with hmda-platform#264 and new router

--- src/js/InstitutionStatus.jsx
+++ src/js/InstitutionStatus.jsx
@@ -1,8 +1,6 @@
 var React = require('react');
 var Link = require('react-router').Link;
 var Resubmit = require('./Resubmit.jsx');
-var EditReports = require('./EditReports.jsx');
-
 
 var InstitutionStatus = React.createClass({
 
@@ -11,45 +9,49 @@ var InstitutionStatus = React.createClass({
   },
 
   getStatusText: function(statusCode){
+    var year = this.props.year;
+    var id = this.props.institution.id;
+    var appLink = '/' + year + '/' + id;
+    var statusLink = <Link to={appLink}>View filing status</Link>
     var statusText = null;
-    var resubmit = null;
-    var editReports = null;
+    var resubmit = <Resubmit {...this.props}/>;
 
     switch(statusCode){
       case 0:
-        statusText = <p><Link to={'/upload/' + this.props.institution.name}>Begin filing now</Link>.</p>
-        break;
-      case 1:
-        statusText = <p>Your file is being processed. You can <Link to={'/progress/' + this.props.institution.name}>view progress</Link>.</p>
+        statusLink = <Link to={appLink}>Begin filing</Link>
+        resubmit = null;
         break;
       case 2:
-        statusText = <p>All checks complete, but with <Link to={'/edits/' + this.props.institution.name + '#syntactical'}>syntactical</Link> and <Link to={'/edits/' + this.props.institution.name + '#validity'}>validity</Link> errors. These errors must be corrected before verifying data quality.</p>
-        break;
       case 3:
-        statusText = <p>All checks complete. <Link to={'/errors/' + this.props.institution.name}>Verify quality and macro checks</Link>.</p>
-        break;
       case 4:
-        statusText = <p>All checks and verification complete. <Link to={'/summary/' + this.props.institution.name}>View the summary report</Link> or <Link to={'/sign/' + this.props.institution.name}>sign and submit</Link>.</p>
-        break;
       case 5:
-        statusText = <p>All checks complete, verified, and signed. <Link to={'/summary/' + this.props.institution.name}>View the summary report</Link>.</p>
+      case 6:
+        statusText = <p>Your file is being processed.</p>
+        break;
+      case 7:
+        statusText = <p>All checks complete, but with syntactical and validity edits. These edits must be corrected before verifying data quality.</p>
+        break;
+      case 8:
+        statusText = <p>All checks complete. You must verify quality and macro edits before continuing.</p>
+        break;
+      case 9:
+      case 10:
+        statusText = <p>All checks and verification complete. Review the summary report and sign the submission.</p>
+        break;
+      case 11:
+        statusText = <p>All checks complete, verified, and signed.</p>
+        statusLink = <Link to={appLink}>View completed filing</Link>
         break;
       default:
         throw new Error('Unexpected institution status');
     }
 
-    if(statusCode > 1){
-      editReports = <EditReports {...this.props}/>;
-      if(statusCode < 5){
-        resubmit = <Resubmit {...this.props}/>;
-      }
-    }
 
     return (
       <div>
         {statusText}
+        {statusLink}
         {resubmit}
-        {editReports}
       </div>
     )
   },
@@ -58,6 +60,7 @@ var InstitutionStatus = React.createClass({
     return (
       <div className="InstitutionStatus">
         <h3>{this.props.institution.name}</h3>
+        {this.getStatusText(this.props.institution.status.code)}
       </div>
     )
   }
