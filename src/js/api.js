c0c4ba7669ff90ab9f8f0c371b5ced605ad6e761
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-05-12 01:43:33 -0700

    api updates to remove repeated/brittle code.. still somewhat doomed

--- src/js/api.js
+++ src/js/api.js
@@ -8,28 +8,54 @@ function makeResponder(cb){
   }
 }
 
+function getHandler(url, cb, suffix){
+  if(typeof url === 'function'){
+    cb = url;
+    url = makeUrl(suffix);
+  }
+
+  superagent.get(url).end(makeResponder(cb));
+}
+
+function postHandler(url, cb, suffix){
+  if(typeof url === 'function'){
+    cb = url;
+    url = makeUrl(suffix);
+  }
+
+  superagent.post(url).end(makeResponder(cb));
+}
+
+function makeUrl(suffix){
+  var locationObj = parseLocation();
+  var url = '/api'
+  if(locationObj.year) url+= '/years/' + locationObj.year;
+  if(locationObj.institution) url+= '/institutions/' + locationObj.institution;
+  if(locationObj.submission) url+= '/submissions/' + locationObj.submission;
+  if(suffix) url += suffix;
+  return url;
+}
+
+function parseLocation(){
+  var pathParts = location.pathname.split('/');
+  return {year: pathParts[1], institution:  pathParts[2], submission: pathParts[3]}
+ }
+
 module.exports = {
+
  getInstitutions: function(cb){
-   superagent.get('api/institutions').end(makeResponder(cb));
+   return getHandler('api/institutions', cb);
  },
 
- getInstitution: function(cb){
-   var locationObj = this.parseLocation();
-   superagent.get('/api/years/' + locationObj.year + '/institutions/' + locationObj.institution).end(makeResponder(cb));
+ getInstitution: function(url, cb){
+   return getHandler(url, cb);
  },
 
- postSubmissions: function(cb){
-   var locationObj = this.parseLocation();
-   superagent.post( '/api/years/' + locationObj.year + '/institutions/' + locationObj.institution + '/submissions').end(makeResponder(cb));
+ postSubmissions: function(url, cb){
+   return postHandler(url, cb, '/submissions');
  },
 
- getEdits: function(cb){
-   var locationObj = this.parseLocation();
-   superagent.get('/api/years/' + locationObj.year + '/institutions/' + locationObj.institution + '/submissions/' + locationObj.submission + '/edits').end(makeResponder(cb));
- },
-
- parseLocation: function(){
-   var pathParts = location.pathname.split('/');
-   return {year: pathParts[1], institution:  pathParts[2], submission: pathParts[3]}
+ getEdits: function(url, cb){
+   return getHandler(url, cb, '/edits');
  }
 }
