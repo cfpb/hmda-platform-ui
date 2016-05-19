d9654e417a1dbb26d816cd1fb94dc1be6bd83500
Andrew <awolfe76@gmail.com>
2016-05-19 10:38:25 -0400

    Created Signature.jsx
    Signature.jsx will allow filers to confirm and sign their submission after all edits have been verified.

---
+++ src/js/Signature.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var Signature = React.createClass({
+  propTypes: {
+
+  },
+
+  render: function() {
+    return (
+      <div className="Signature full">
+        You cannot sign this yet.
+      </div>
+    )
+  }
+});
+
+module.exports = Signature;
