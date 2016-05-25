856347b51ed1c45cee0ac36b6ed8b2dfed277915
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-05-25 14:46:53 -0700

    data reorg/filter impl

--- src/js/EditsContainer.jsx
+++ src/js/EditsContainer.jsx
@@ -1,44 +1,112 @@
 var React = require('react');
 var api = require('./api');
+var EditsSelector = require('./EditsSelector.jsx');
 var EditsGrouped = require('./EditsGrouped.jsx');
 var EditsMacro = require('./EditsMacro.jsx');
 var EditsHeaderDescription = require('./EditsHeaderDescription.jsx');
 
 var EditsContainer = React.createClass({
-  getInitialState: function() {
+  getInitialState: function(){
     return {
-      edits: {
-        syntactical: {edits: []},
-        validity: {edits: []},
-        quality: {edits: []},
-        macro: {edits: []}
-      }
+      syntactical: {edits: []},
+      validity: {edits: []},
+      quality: {edits: []},
+      macro: {edits: []},
+      lars: []
+      groupByRow: false
     }
   },
 
-  componentWillMount: function() {
+  componentWillMount: function(){
+    this.getEditsByGrouping();
+  },
+
+  getEditsByGrouping: function(){
     var self = this;
-    api.getEdits(function(editObj){
-      self.setState({
-        edits: editObj
-      });
+    var fn = self.state.groupByRow ? api.getEditsByRow : api.getEditsByType;
+    fn(function(editObj){
+      self.setState(editObj);
     });
   },
 
-  render: function() {
+  updateGrouping: function(groupByRow){
+    this.setState({groupByRow: groupByRow});
+    if(groupByRow) this.populateLars();
+    else this.populateEditTypes();
+  },
+
+  populateLars: function(){
+    var larsObj = {};
+    var lars = [];
+
+    ['syntactical', 'validity', 'quality'].forEach(function(type){
+      var edits = this.state[type].edits;
+
+      edits.forEach(function(editWrapper){
+        editWrapper.lars.forEach(function(larWrapper){
+          var editObj = {edit: editWrapper.edit, type: type};
+          if(type === 'quality') editObj.verification = larWrapper.verification;
+          var id = larWrapper.lar.loanId
+          larsObj[id] = larsObj[id] || {lar: larWrapper.lar, edits: []};
+          larsObj[id].edits.push(editObj);
+        });
+      });
+    });
+
+    Object.keys(larsObj).forEach(function(lar){
+      lars.push(larsObj[lar]);
+    });
+
+    this.setState({lars: lars});
+  },
+
+  populateEditTypes: function(){
+    var edits = {
+      syntactical: {type: 'syntactical', edits: []},
+      validity: {type: 'validity', edits: []},
+      quality: {type: 'quality', edits: []}
+    };
+
+    var editObj = {
+      syntactical: {edits: {}},
+      validity: {edits: {}},
+      quality: {edits: {}}
+    };
+
+    this.state.lars.forEach(function(larWrapper){
+      larWrapper.edits.forEach(function(editWrapper){
+        var editList = editObj[editWrapper.type].edits;
+        var currEdit = editList[editWrapper.edit];
+        var larObj = {lar: larWrapper.lar}
+        if(editWrapper.type === 'quality') larObj.verification = editWrapper.verification;
+
+        editList[editWrapper.edit] = currEdit || {edit: editWrapper.edit, lars: []};
+        currEdit.lars.push(larObj);
+      });
+    });
+
+    Object.keys(editObj).forEach(function(type){
+      Object.keys(editObj[type].edits).forEach(function(edit){
+        edits[type].edits.push(edit);
+      });
+    });
+
+    this.setState(edits);
+  },
+
+  renderByType: function(){
+  
+  },
+
+  renderByRow: function(){
+    
+  },
+
+  render: function(){
     return (
-      <div className="EditsContainer">
-        <EditsHeaderDescription>Syntactical</EditsHeaderDescription>
-        <EditsGrouped id="syntactical" edits={this.state.edits.syntactical.edits} />
-
-        <EditsHeaderDescription>Validity</EditsHeaderDescription>
-        <EditsGrouped id="validity" edits={this.state.edits.validity.edits} />
-
-        <EditsHeaderDescription>Quality</EditsHeaderDescription>
-        <EditsGrouped id="quality" edits={this.state.edits.quality.edits} />
-
-        <EditsHeaderDescription>Macro</EditsHeaderDescription>
-        <EditsMacro id="macro" edits={this.state.edits.macro} />
+      <div className="EditsContainer two-third">
+        <EditsSelector callback={this.updateGrouping}/>
+        {this.renderEditsBody()}
       </div>
     )
   }
