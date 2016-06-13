f2a17ae781a4835bf286725d719928c510f5eae2
Andrew <awolfe76@gmail.com>
2016-06-13 09:45:56 -0400

    q029 testing

---
+++ __tests__/Q029.js
@@ -0,0 +1,35 @@
+jest.dontMock('../src/js/EditsQ029.jsx');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+
+var EditsQ029 = require('../src/js/EditsQ029.jsx');
+var fs = require('fs');
+
+var edits = JSON.parse(fs.readFileSync('./server/json/q029.json'));
+
+describe('Q029 edits', function(){
+  var changeHandlerTrue = function(e){
+    expect(e.target.checked).toBeTruthy();
+  };
+
+  var changeHandlerFalse = function(e){
+    expect(e.target.checked).toBeFalsy();
+  };
+
+  var editsQ029 = TestUtils.renderIntoDocument(<EditsQ029 group={edits.edits}/>)
+  var editsQ029Node = ReactDOM.findDOMNode(editsQ029);
+
+  it('renders the editsQ029 component', function(){
+    expect(editsQ029Node).toBeDefined();
+  });
+
+  it('passes through props', function(){
+    expect(editsQ029.props.group).toEqual(edits.edits);
+  });
+
+  it('renders the select elements', function(){
+    expect(TestUtils.scryRenderedDOMComponentsWithTag(editsQ029, 'select').length).toEqual(4);
+  });
+});
