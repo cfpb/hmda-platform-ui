57b31a102258b2ec734921e9e85d75a860c0b31f
Andrew <awolfe76@gmail.com>
2016-06-16 09:21:22 -0400

    create the summary component

---
+++ src/js/Summary.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var Summary = React.createClass({
+
+  propTypes: {
+    // TODO:
+  },
+
+  render: function(){
+    <div class="Summary">
+
+    </div>
+  }
+});
+
+module.exports = Summary;
