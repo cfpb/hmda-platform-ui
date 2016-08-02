e8f366d4ebc96e03e988533a597f999267af3aec
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-08-01 22:15:09 -0700

    Update action creators to include per institution functions

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -6,6 +6,9 @@ export const RECEIVE_NEW_SUBMISSION = 'RECEIVE_NEW_SUBMISSION'
 export const REQUEST_INSTITUTIONS = 'REQUEST_INSTITUTIONS'
 export const RECEIVE_INSTITUTIONS = 'RECEIVE_INSTITUTIONS'
 
+export const REQUEST_INSTITUTION = 'REQUEST_INSTITUTION'
+export const RECEIVE_INSTITUTION = 'RECEIVE_INSTITUTION'
+
 export const REQUEST_UPLOAD = 'REQUEST_UPLOAD'
 export const RECEIVE_UPLOAD = 'RECEIVE_UPLOAD'
 
@@ -28,11 +31,44 @@ export function receiveInstitutions(data) {
   }
 }
 
+export function requestInstitution() {
+  return {
+    type: REQUEST_INSTITUTION
+  }
+}
+
+export function receiveInstitution(data) {
+  return {
+    type: RECEIVE_INSTITUTION,
+    institutions: data
+  }
+}
+
 export function getInstitutions() {
   return dispatch => {
     dispatch(requestInstitutions())
     return api.getInstitutions()
       .then(json => dispatch(receiveInstitutions(json)))
+      .then(json => getEachInstitution(json)(dispatch))
+      .catch(err => console.log(err))
+  }
+}
+
+export function getEachInstitution(institutions) {
+  return dispatch => {
+    institutions.forEach( institution => {
+      if (institution.status === 'active') {
+        getInstitution(institution)(dispatch)
+      }
+    })
+  }
+}
+
+export function getInstitution(institution) {
+  return dispatch => {
+    dispatch(requestInstitution())
+    api.getInstitution(institution)
+      .then(json => receiveInstitution(json))
       .catch(err => console.log(err))
   }
 }
