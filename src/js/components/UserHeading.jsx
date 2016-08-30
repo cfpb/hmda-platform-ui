8378f54d00ca221b97496f9f828ca2237d44d1e1
Andrew <awolfe76@gmail.com>
2016-08-30 14:49:18 -0400

    home link updated to es6, including test

--- src/js/components/UserHeading.jsx
+++ src/js/components/UserHeading.jsx
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 
 export default class UserHeading extends Component {
   render() {
-    if(!this.props.userName) return null
+    //if(!this.props.userName) return null
 
     let headingText = 'Welcome to the ' + props.period + ' HMDA filing, ' + props.userName
 
