4db29c3b78be6e5e22fd377a373c8e7182bcd677
Andrew <awolfe76@gmail.com>
2016-09-07 20:09:02 -0400

    tests updated to ES6 but failing, will fix in a future PR

--- __tests__/IRSReport.js
+++ __tests__/IRSReport.js
@@ -1,69 +1,71 @@
-jest.dontMock('../src/js/IRSReport.jsx');
+jest.dontMock('../src/js/IRSReport.jsx')
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-var TestUtils = require('react-addons-test-utils');
+import DivisionHeader from '../src/js/components/DivisionHeader.jsx'
+import Wrapper from './Wrapper.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
 
-var IRSReport = require('../src/js/IRSReport.jsx');
-var fs = require('fs');
-var api = require('../src/js/api');
-
-var irsJSON = JSON.parse(fs.readFileSync('./server/json/irs.json'));
+/*
+var fs = require('fs')
+var api = require('../src/js/api')
+var irsJSON = JSON.parse(fs.readFileSync('./server/json/irs.json'))
 
 api.getIRS = jest.fn(function(cb){
-  cb(null, irsJSON);
-});
+  cb(null, irsJSON)
+})
 
 api.postIRS = jest.fn(function(cb, data){
-  var code = data.verified ? 11 : 10;
-  cb(null, {status: {code: code, message: ''}});
-});
+  var code = data.verified ? 11 : 10
+  cb(null, {status: {code: code, message: ''}})
+})
+*/
 
-describe('irs report', function(){
-
-  function uncheckedToggle(err, status){
-    expect(status.status.code).toBe(11);
+describe('irs report', () => {
+  const uncheckedToggle = (err, status) => {
+    expect(status.status.code).toBe(11)
   }
+  const irsReport = TestUtils.renderIntoDocument(
+    <IRSReport />)
+  const irsReportNode = ReactDOM.findDOMNode(irsReport)
 
-  var irsReport = TestUtils.renderIntoDocument(<IRSReport checked={false} appStatus={{set:uncheckedToggle}}/>);
-  var irsReportNode = ReactDOM.findDOMNode(irsReport);
+  it('renders the irs report component', () => {
+    expect(irsReportNode).toBeDefined()
+  })
 
-  it('renders the irs report component', function(){
-    expect(irsReportNode).toBeDefined();
-  });
+  it('creates the correct number of rows', () => {
+    expect(TestUtils.scryRenderedDOMComponentsWithTag(irsReport, 'tr').length).toEqual(4)
+  })
 
-  it('creates the correct number of rows', function(){
-    expect(TestUtils.scryRenderedDOMComponentsWithTag(irsReport, 'tr').length).toEqual(4);
-  });
+  it('contains the checkbox input', () => {
+    expect(TestUtils.scryRenderedDOMComponentsWithTag(irsReport, 'input').length).toEqual(1)
+  })
 
-  it('contains the checkbox input', function(){
-    expect(TestUtils.scryRenderedDOMComponentsWithTag(irsReport, 'input').length).toEqual(1);
-  });
+  it('toggles an unchecked checkbox to checked', () => {
+    const checkbox = TestUtils.findRenderedDOMComponentWithTag(irsReport, 'input')
 
-  it('toggles an unchecked checkbox to checked', function(){
-
-    var checkbox = TestUtils.findRenderedDOMComponentWithTag(irsReport, 'input');
-    expect(checkbox.checked).toBeFalsy();
+    expect(checkbox.checked).toBeFalsy()
 
     TestUtils.Simulate.change(
       checkbox,
       {'target': {'checked': true}}
-    );
-  });
+    )
+  })
 
-  it('toggles a checked checkbox to unchecked', function(){
-
-    function checkedToggle(err, status){
-      expect(status.status.code).toBe(10);
+  it('toggles a checked checkbox to unchecked', () => {
+    const checkedToggle = (err, status) => {
+      expect(status.status.code).toBe(10)
     }
+    const irsReportChecked = TestUtils.renderIntoDocument(
+      <IRSReport />
+    )
+    const checkbox = TestUtils.findRenderedDOMComponentWithTag(irsReportChecked, 'input')
 
-    var irsReportChecked = TestUtils.renderIntoDocument(<IRSReport checked={true} appStatus={{set: checkedToggle}}/>);
-    var checkbox = TestUtils.findRenderedDOMComponentWithTag(irsReportChecked, 'input');
-    expect(checkbox.checked).toBeTruthy();
+    expect(checkbox.checked).toBeTruthy()
 
     TestUtils.Simulate.change(
       checkbox,
       {'target': {'checked': false}}
-    );
-  });
-});
+    )
+  })
+})
