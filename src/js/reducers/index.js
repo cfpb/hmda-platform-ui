a179187abc40109c6840a890337867b8782f8a41
Andrew <awolfe76@gmail.com>
2016-09-20 13:52:50 -0400

    update reducers to include the summary

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -269,6 +269,33 @@ export const signature = (state = defaultSignature, action) => {
   }
 }
 
+const defaultSummary = {
+  isFetching: false,
+  respondent: {},
+  file: {}
+}
+
+export const summary = (state = defaultSummary, action) => {
+  switch (action.type) {
+    case REQUEST_SUMMARY:
+      return {
+        ...state,
+        isFetching: true
+      }
+
+    case RECEIVE_SUMMARY:
+      return {
+        ...state,
+        isFetching: false,
+        respondent: action.respondent,
+        file: action.file
+      }
+
+    default:
+      return state
+  }
+}
+
 export default combineReducers({
   institutions,
   filings,
@@ -276,5 +303,6 @@ export default combineReducers({
   upload,
   edits,
   irs,
-  signature
+  signature,
+  summary
 })
