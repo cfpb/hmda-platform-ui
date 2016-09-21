91f16640bc45013583f126830a2137c4d79ed27b
Andrew <awolfe76@gmail.com>
2016-09-21 10:36:33 -0400

    updates to ES6 and changes to props, needs status instead of just the code due to redux implementation

--- src/js/RefileWarning.jsx
+++ src/js/RefileWarning.jsx
@@ -1,44 +1,51 @@
-var React = require('react');
-var Link = require('react-router').Link;
-var api = require('./api');
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import { parseLocation } from '../api'
 
-var refileText = 'Syntactical and validity edits require file resubmission.'
-var validateText = 'Quality and macro edits must be validated before continuing.'
+const refileText = 'Syntactical and validity edits require file resubmission.'
+const validateText = 'Quality and macro edits must be validated before continuing.'
 
-var RefileWarning = React.createClass({
+const getText = (props) => {
+  let textToRender = null;
+  let refileLink = null;
 
-  propTypes: {
-    code: React.PropTypes.number
-  },
-  getText: function(){
-    var textToRender = null;
-    var refileLink = null;
+  if(props.code === 7) {
+    textToRender = refileText;
+    refileLink = getRefileLink();
+  } else if(props.code === 8) {
+    textToRender = validateText;
+  }
 
-    if(this.props.code === 7){
-      textToRender = refileText;
-      refileLink = this.getRefileLink();
-    }else if(this.props.code === 8){
-      textToRender = validateText;
-    }
+  return <h3><span className="cf-icon cf-icon-error cf-icon__3x"></span><span className="refile-text">{textToRender}{refileLink}</span></h3>
+}
 
-    return <h3><span className="cf-icon cf-icon-error cf-icon__3x"></span><span className="refile-text">{textToRender}{refileLink}</span></h3>
-  },
+const getRefileLink = () => {
+  const location = parseLocation()
+  const href = `/${location.id}/${location.period}/${(+location.submission + 1)}`
+  //const href = '/' + location.id + '/' + location.period + '/' + (+location.submission + 1);
+  return <Link to={href}> Refile here.</Link>
+}
 
-  getRefileLink: function(){
-    var location = api.parseLocation();
-    var href = '/' + location.id + '/' + location.period + '/' + (+location.submission + 1);
-    return <Link to={href}> Refile here.</Link>
-  },
+const RefileWarning = (props) => {
+  if (props.code > 8) return null
 
-  render: function(){
-    if(this.props.code > 8) return null;
-
-    return <div className="RefileWarning">
+  return (
+    <div className="RefileWarning">
       <div>
-        {this.getText()}
+        {getText()}
       </div>
     </div>
-  }
-});
+  )
+}
 
-module.exports = RefileWarning;
+RefileWarning.propTypes = {
+  status: PropTypes.object
+}
+
+RefileWarning.defaultProps = {
+  status: {
+    code: null
+  }
+}
+
+export default RefileWarning
