1be507af5992c74c3fc8a6657988ef583f7f7d20
Andrew <awolfe76@gmail.com>
2016-09-30 09:37:42 -0400

    add classes for usdws

--- src/js/components/UploadForm.jsx
+++ src/js/components/UploadForm.jsx
@@ -3,9 +3,9 @@ import Progress from './Progress.jsx'
 
 const Upload = (props) => (
   <div className="UploadForm usa-grid-full">
-    <form encType="multipart/form-data" onSubmit={e => props.handleSubmit(e, props.file)}>
+    <form className="usa-form" encType="multipart/form-data" onSubmit={e => props.handleSubmit(e, props.file)}>
       <input id="hmdaFile" name="hmdaFile" type="file" onChange={props.setFile}></input>
-      <input className="btn" id="uploadButton" name="uploadButton" type="submit" value="Upload"></input>
+      <input className="usa-button" id="uploadButton" name="uploadButton" type="submit" value="Upload"></input>
     </form>
     {props.file
       ? <Progress progress={props.bytesUploaded} total={props.file.size} units="bytes" descriptor="uploaded"/>
