5467b44d6dca26f7fa5f861a2dba8ffc77d78ca1
Andrew <awolfe76@gmail.com>
2016-10-14 14:31:17 -0400

    update test

--- __tests__/actions.js
+++ __tests__/actions.js
@@ -211,7 +211,11 @@ describe('actions', () => {
   })
 
   it('creates an action to signal current submission data has been received', () => {
-    const data = {answer: 42}
+    const data = {
+      id: {
+        sequenceNumber: 2
+      }
+    }
     expect(actions.receiveSubmission(data)).toEqual({
       type: types.RECEIVE_SUBMISSION,
       ...data
