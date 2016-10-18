0145e7d78a55255a1858535998348061d1794fc7
Andrew <awolfe76@gmail.com>
2016-10-18 16:42:45 -0400

    disable input and show warning for signature until status is greater than 10 (IRS needs to be verified)

--- src/js/components/Signature.jsx
+++ src/js/components/Signature.jsx
@@ -12,10 +12,25 @@ const showReceipt = (props) => {
   )
 }
 
+const showWarning = (props) => {
+  if(props.status.code > 10) return null
+
+  return (
+    <div className="usa-alert usa-alert-warning">
+      <div className="usa-alert-body">
+        <h3 className="usa-alert-heading">You can not sign your submission until the IRS report has been verified.</h3>
+      </div>
+    </div>
+  )
+}
+
 const Signature = (props) => {
   const isChecked = props.status.code > 12 ? true : false
+  const isDisabled = props.status.code > 11 ? false : true
+
   return (
     <div className="Signature">
+      {showWarning(props)}
       <ul className="usa-unstyled-list">
         <li>
           <input id="signature"
@@ -23,7 +38,8 @@ const Signature = (props) => {
             type="checkbox"
             value="signature"
             onChange={e => props.onSignatureClick(e.target.checked)}
-            checked={isChecked} />
+            checked={isChecked}
+            disabled={isDisabled} />
           <label htmlFor="signature">I am an authorized representative of my institution with knowledge of the data submitted and can certify to the accuracy and completeness of the data submitted.</label>
         </li>
       </ul>
