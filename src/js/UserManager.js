abc2ba5729239bf14953e53c9a0414a971b238ed
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-10-31 16:15:17 -0700

    silent renew

--- src/js/UserManager.js
+++ src/js/UserManager.js
@@ -4,6 +4,8 @@ const userManager = createUserManager({
   authority: 'https://192.168.99.100:8443/auth/realms/hmda',
   client_id: 'hmda-api',
   redirect_uri: 'http://192.168.99.100/oidc-callback',
+  silent_redirect_uri: 'http://192.168.99.100/silent_renew.html',
+  automaticSilentRenew: true,
   scope: 'openid profile email',
   response_type: 'id_token token'
 })
