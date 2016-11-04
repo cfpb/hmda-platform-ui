5a207fb7697094862b1237566eba21bce51e7a6b
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-11-04 09:44:41 -0700

    localStorage

--- src/js/UserManager.js
+++ src/js/UserManager.js
@@ -1,4 +1,5 @@
 import { createUserManager } from 'redux-oidc'
+import { WebStorageStateStore } from 'oidc-client'
 
 const userManager = createUserManager({
   authority: 'https://192.168.99.100:8443/auth/realms/hmda',
@@ -7,7 +8,10 @@ const userManager = createUserManager({
   silent_redirect_uri: 'http://192.168.99.100/silent_renew.html',
   automaticSilentRenew: true,
   scope: 'openid profile',
-  response_type: 'id_token token'
+  response_type: 'id_token token',
+  store: new WebStorageStateStore({store: localStorage})
 })
+
+userManager.events.addUserLoaded(user => console.log('USER LOADED', user))
 
 export default userManager
