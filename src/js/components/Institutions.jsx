e12de96bddd34a74b9a3a82d7e75f72cbd45f3e6
Andrew <awolfe76@gmail.com>
2016-11-23 14:53:04 -0500

    rendering institutions in a cleaner way, along with relevant times of submissions

--- src/js/components/Institutions.jsx
+++ src/js/components/Institutions.jsx
@@ -1,29 +1,62 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 import UserHeading from '../components/UserHeading.jsx'
+import moment from 'moment'
+
+const renderLabel = (status) => {
+  let bgColor = 'bg-color-red'
+  if(status.code === 2) {
+    color = ''
+  }
+  if(status.code === 3) {
+    bgColor = 'bg-color-green'
+  }
+  return <span className={`usa-label ${bgColor}`}>{status.message}</span>
+}
+
+const renderTiming = (start, end) => {
+  let timing = <p className="text-gray usa-text-small text-uppercase"><strong className="text-primary">Started</strong> {moment(start).fromNow()}</p>
+
+  if(start === 0) timing = <p className="text-gray usa-text-small text-uppercase"><strong className="text-secondary">Not started</strong></p>
+
+  if(end !== 0) timing = <p className="text-gray usa-text-small text-uppercase"><strong className="text-green">Completed</strong> {moment(end).format('MMMM Do')}</p>
+
+  return timing
+}
+
+const renderStatus = (code, institutionName, institutionId, period) => {
+  let status = <p>{institutionName} hasn't started a filing yet. You can <Link to={`/${institutionId}/${period}`}>begin filing</Link> now.</p>
+
+  switch(code) {
+    case 2:
+      status = <p>{institutionName}'s file is being processed. You can <Link to={`/${institutionId}/${period}`}>view the progress</Link> now.</p>
+    case 3:
+      status = <p>{institutionName}'s file has been processed. You can <Link to={`/${institutionId}/${period}`}>review edits and sign</Link> {institutionName}'s submission now.</p>
+    case 4:
+      status = <p>{institutionName}'s filing is complete and signed. You can <Link to={`/${institutionId}/${period}`}>review</Link> {institutionName}'s signed submission now.</p>
+  }
+
+  return status
+}
 
 export default class Institution extends Component {
-
   render() {
-    console.log('institution component')
-    console.log(this.props)
     var self = this
     return (
-    <div className="Institution">
+    <div className="Institutions">
       <UserHeading period="2017" userName={this.props.user.profile.name} />
       {this.props.institutions.map((institution, i) => {
         return (
-        <div key={i}>
-          <h3>{institution.name}</h3>
+        <div key={i} className="institution">
           {self.props.filings.filter(
             filing => filing.institutionId === institution.id
           ).map((filing, i) => {
             return (
             <div key={i}>
-              <p>id: {filing.period}</p>
-              <p>institutionId: {filing.institutionId}</p>
-              <p>status: {filing.status.message}</p>
-              <p><Link to={`/${filing.institutionId}/${filing.period}`}>View filing</Link></p>
+              {renderTiming(filing.start, filing.end)}
+              <h2>{institution.name}</h2>
+              {/*{renderLabel(filing.status)}*/}
+              {renderStatus(filing.status.code, institution.name, filing.institutionId, filing.period)}
             </div>
             )
           })}
