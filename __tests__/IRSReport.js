ab562d574e395911cdf7e0618c42a6653c625600
Andrew <awolfe76@gmail.com>
2016-12-19 13:56:00 -0500

    fix IRS tests

--- __tests__/IRSReport.js
+++ __tests__/IRSReport.js
@@ -17,7 +17,7 @@ describe('IRS report', () => {
   const onIRSClick = jest.fn()
   const irsReport = TestUtils.renderIntoDocument(
     <Wrapper>
-      <IRSReport msas={irsJSON.msas} receipt={irsJSON.receipt} timestamp={irsJSON.timestamp} status={status} onIRSClick={onIRSClick}/>
+      <IRSReport msas={irsJSON.msas} receipt={irsJSON.receipt} timestamp={irsJSON.timestamp} status={status} />
     </Wrapper>
   )
   const irsReportNode = ReactDOM.findDOMNode(irsReport)
@@ -28,47 +28,5 @@ describe('IRS report', () => {
 
   it('creates the correct number of rows', () => {
     expect(TestUtils.scryRenderedDOMComponentsWithTag(irsReport, 'tr').length).toEqual(4)
-  })
-
-  it('contains the checkbox input', () => {
-    expect(TestUtils.scryRenderedDOMComponentsWithTag(irsReport, 'input').length).toEqual(1)
-  })
-
-  it('does NOT render the confirmation', () => {
-    expect(TestUtils.scryRenderedDOMComponentsWithClass(irsReport, 'confirmation').length).toEqual(0)
-  })
-
-  it('calls the function on change', () => {
-    const checkbox = TestUtils.findRenderedDOMComponentWithTag(irsReport, 'input')
-
-    expect(checkbox.checked).toBeFalsy()
-
-    TestUtils.Simulate.change(checkbox, {
-      'target': {
-        'checked': true
-      }
-    })
-
-    expect(onIRSClick).toBeCalled()
-  })
-
-  const statusConfirmed = {
-    code: 11,
-    message: ''
-  }
-  const irsReportConfirmed = TestUtils.renderIntoDocument(
-    <Wrapper>
-      <IRSReport msas={irsJSON.msas} receipt={irsJSON.receipt} timestamp={irsJSON.timestamp} status={statusConfirmed} onIRSClick={onIRSClick}/>
-    </Wrapper>
-  )
-  const irsReportConfirmedNode = ReactDOM.findDOMNode(irsReportConfirmed)
-
-  it('renders the confirmation', () => {
-    expect(TestUtils.findRenderedDOMComponentWithClass(irsReportConfirmed, 'usa-alert-success')).toBeTruthy()
-  })
-
-  it('has the checkbox checked', () => {
-    const checkboxChecked = TestUtils.findRenderedDOMComponentWithTag(irsReportConfirmed, 'input')
-    expect(checkboxChecked.checked).toBeTruthy()
   })
 })
