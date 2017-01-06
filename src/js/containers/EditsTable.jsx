deb41d087a311264c69158311ffca24e745e609e
Andrew <awolfe76@gmail.com>
2017-01-05 20:28:30 -0500

    updates to handle rendering edits to match the api; also cleans it up a bit and based some things on the 'type' instead of the objects

--- src/js/containers/EditsTable.jsx
+++ src/js/containers/EditsTable.jsx
@@ -2,19 +2,13 @@ import React from 'react'
 
 import EditsTableRow from '../components/EditsTableRow.jsx'
 
-const rowForEachLarTypes = ['syntactical', 'validity', 'quality']
-
-const rowForEachLar = (val) => rowForEachLarTypes.indexOf(val) !== -1
-
 const renderHeader = (props) => {
-  let row = props.lars ? props.lars[0] : props.data[0]
-  if (rowForEachLar(props.type)) row = row.lar
-
+  let row = (props.type !== 'macro') ? props.edits.rows[0].row : props.edits[0]
   return (
     <tr>
       {
         Object.keys(row).map((header, i) => {
-          if(header === 'loanId') header = 'Row ID'
+          if(header === 'rowId') header = 'Row ID'
           if(header === 'edit') header = 'Edit ID'
           if(header === 'justifications') header = 'Justifications'
           return <th key={i}>{header}</th>
@@ -24,45 +18,31 @@ const renderHeader = (props) => {
   )
 }
 
-const renderTS = (ts) => {
-  if (ts) {
-    return (
-      <tr>
-        <td>Transmittal Sheet</td>
-      </tr>
-    )
-  }
-}
-
 const renderBody = (props) => {
-  // if there is no props.lars (macro)
-  if (!props.lars) {
-    return props.data.map((macro, i) => {
+  if (props.type === 'macro') {
+    return props.edits.map((macro, i) => {
       return <EditsTableRow row={macro} key={i}/>
     })
   }
-  return props.lars.map((row, i) => {
-    if(row.lar) row = row.lar
-    return <EditsTableRow row={row} key={i}/>
+  return props.edits.rows.map((row, i) => {
+    return <EditsTableRow row={row.row} key={i}/>
   })
 }
 
 const EditsTable = (props) => {
-  let editCount = !props.lars ? props.data.length : props.lars.length
-  editCount = props.ts ? (editCount + 1) : (editCount)
-
+  const edits = props.edits
+  const { edit, description } = props.edits
   return (
     <div className="EditsTable bg-color-white">
       <table width="100%" className="margin-top-1">
         <caption>
-          <h3>{props.label ? `${props.label} - ${editCount}`: null}</h3>
-          <p>{props.desc? `${props.desc}`: null}</p>
+          <h3>{edit ? `${edit} - ${length}`: null}</h3>
+          <p>{description ? `${description}`: null}</p>
         </caption>
         <thead>
           {renderHeader(props)}
         </thead>
         <tbody>
-          {renderTS(props.ts)}
           {renderBody(props)}
         </tbody>
       </table>
