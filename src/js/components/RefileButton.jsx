d8c689222a8281252592c49e46f2fce3dc71c97c
Andrew <awolfe76@gmail.com>
2017-02-17 15:47:21 -0500

    set positioning on refile button based on passing a prop

--- src/js/components/RefileButton.jsx
+++ src/js/components/RefileButton.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 const RefileButton = (props) => {
-  return <a className="usa-button usa-button-secondary usa-text-small"
+  return <a className={`RefileButton ${props.class} usa-button usa-button-secondary usa-text-small `}
     onClick={(e)=>{
       e.preventDefault()
       props.showConfirmModal(props.id, props.filing, props.code)
-    }}
-    >Refile</a>
+    }}>Refile</a>
 }
 
 RefileButton.propTypes = {
   showConfirmModal: React.PropTypes.func.isRequired,
   id: React.PropTypes.string.isRequired,
   filing: React.PropTypes.string.isRequired,
-  code: React.PropTypes.number.isRequired
+  code: React.PropTypes.number.isRequired,
+  class: React.PropTypes.string
 }
 
 export default RefileButton
