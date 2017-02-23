22d5f25ed2455eb60de788a7ecb803bf7543b47b
Andrew <awolfe76@gmail.com>
2017-02-23 12:05:33 -0500

    better coverage

--- src/js/components/EditsTableCell.jsx
+++ src/js/components/EditsTableCell.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 
 import JustificationSelector from '../containers/JustificationSelector.jsx'
 
-const renderCell = (props) => {
+export const renderCell = (props) => {
   const { field, cell, keyField } = props
   if(field === 'justifications') return <JustificationSelector edit={keyField} justifications={cell}/>
   if(field === 'verified') return 'veriholder'
