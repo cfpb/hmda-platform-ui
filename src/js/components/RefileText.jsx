060d5a71ca5a4f3b819eed3c5a177c29f2683409
Andrew <awolfe76@gmail.com>
2017-03-15 13:05:33 -0400

    content updates to modal

--- src/js/components/RefileText.jsx
+++ src/js/components/RefileText.jsx
@@ -4,19 +4,29 @@ export const getStatus = (code) => {
   let status
 
   if(code > 7) {
-    status = 'is in progress'
-    if(code === 11) status = 'has already been submitted'
+    status = 'is in progress.'
+    if(code === 11) status = 'has already been submitted.'
   }
 
   const message = status ? <p>The HMDA data for this filing year <strong>{status}</strong>.</p> : null
 
   return message
 }
+
+export const getNewFileMessage = (code) => {
+  if(code === 11) return 'Your new HMDA file will not be submitted until you clear and/or verify all edits and submit the data.'
+
+  return null
+}
+
 const RefileText = (props) => {
   return (
     <div className="RefileText">
       {getStatus(props.code)}
-      <p className="question">Would you like to start the resubmission process?</p>
+      <p className="question">
+        Would you like to restart the filing process?
+        {getNewFileMessage(props.code)}
+      </p>
     </div>
   )
 }
