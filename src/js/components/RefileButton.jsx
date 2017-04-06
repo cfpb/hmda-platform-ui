ea65f947b98db72d14a3f6be60bde7e310f983b3
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-04-06 14:19:15 -0700

    refile warning content update

--- src/js/components/RefileButton.jsx
+++ src/js/components/RefileButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const RefileButton = (props) => {
-  return <a className={`RefileButton ${props.class} usa-button usa-button-secondary usa-text-small `}
+  return <a className={`RefileButton usa-button usa-button-secondary usa-text-small `}
     onClick={(e)=>{
       e.preventDefault()
       props.showConfirmModal(props.id, props.filing, props.code)
@@ -12,8 +12,7 @@ RefileButton.propTypes = {
   showConfirmModal: React.PropTypes.func.isRequired,
   id: React.PropTypes.string.isRequired,
   filing: React.PropTypes.string.isRequired,
-  code: React.PropTypes.number.isRequired,
-  class: React.PropTypes.string
+  code: React.PropTypes.number.isRequired
 }
 
 export default RefileButton
--- src/js/components/RefileWarning.jsx
+++ src/js/components/RefileWarning.jsx
@@ -6,9 +6,11 @@ export const getText = (props) => {
   let textToRender = null
 
   if(props.syntacticalValidityEditsExist) {
-    textToRender = <p className="usa-alert-text">Your file has <strong>syntactical and/or validity edits</strong>. Update your file and select the "Upload a new file" button, or return to the "Institutions" page.</p>
-  } else {
-    textToRender = <p className="usa-alert-text"><strong>Quality</strong> and <strong>macro</strong> edits must be validated before continuing.</p>
+    textToRender = <div><p className="usa-alert-text">Your file has <strong>syntactical and/or validity edits</strong>.</p><p className="usa-alert-text">Update your file and select the "Upload a new file" button, or return to the "Institutions" page.</p></div>
+  } else if(!props.qualityVerified && props.page === 'quality') {
+    textToRender = <div><p className="usa-alert-text">You must verify the quality edits listed below and select the check box to confirm the accuracy of the data.</p><p className="usa-alert-text">If any of the data need to be corrected, please update your file and select the "Upload a new file" button. You will need to begin the filing process again.</p></div>
+  } else if(!props.macroVerified && props.page === 'macro') {
+    textToRender = <div><p className="usa-alert-text">You must verify the macro quality edits listed below and select the check box to confirm the accuracy of the data.</p><p className="usa-alert-text">If any of the data need to be corrected, please update your file and select the "Upload a new file" button. You will need to begin the filing process again.</p></div>
   }
 
   if(props.code === 5) {
@@ -18,34 +20,23 @@ export const getText = (props) => {
   return textToRender
 }
 
-export const getButton = (props) => {
-  let button = null
-
-  if(props.syntacticalValidityEditsExist) {
-    button = <RefileButton />
-  }
-
-  if(props.code === 5) {
-    button = <RefileButton />
-  }
-
-  return button
-}
-
-
 const RefileWarning = (props) => {
   if (props.code > 8) return null
+  if (props.page === 'syntacticalvalidity' && !props.syntacticalValidityEditsExist) return null
 
   let alertClass = 'usa-alert-error'
   if(!props.syntacticalValidityEditsExist && props.code !== 5) {
     alertClass = 'usa-alert-warning'
   }
 
+  const text = getText(props)
+  const button = text ? <RefileButton/> : null
+
   return (
     <div className={`RefileWarning usa-alert ${alertClass}`}>
-      {getButton(props)}
       <div className="usa-alert-body">
-        {getText(props)}
+        {text}
+        {button}
       </div>
     </div>
   )
