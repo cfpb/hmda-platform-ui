4f72219080aa21eb331605e55073d142686a852a
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-05-05 06:12:43 -0700

    test edits table row

--- src/js/components/EditsTableRow.jsx
+++ src/js/components/EditsTableRow.jsx
@@ -3,17 +3,18 @@ import React, { PropTypes } from 'react'
 import EditsTableCell from '../components/EditsTableCell.jsx'
 
 const EditsTableRow = props => {
-  const edit = props.row.edit
+  if(!props.row || !props.fields) return null
+
   const lar = props.row.rowId
   const cells = []
   let cellCount = 0
 
   Object.keys(props.row).forEach((field) => {
-    cells.push(<EditsTableCell keyField={lar||edit} field={field} cell={props.row[field]} key={++cellCount}/>)
+    cells.push(<EditsTableCell keyField={lar} field={field} cell={props.row[field]} key={++cellCount}/>)
   })
 
   Object.keys(props.fields).forEach((field) => {
-    cells.push(<EditsTableCell keyField={lar||edit} field={field} cell={props.fields[field]} key={++cellCount}/>)
+    cells.push(<EditsTableCell keyField={lar} field={field} cell={props.fields[field]} key={++cellCount}/>)
   })
 
   return <tr>{cells}</tr>
@@ -22,11 +23,6 @@ const EditsTableRow = props => {
 EditsTableRow.propTypes = {
   row: PropTypes.object,
   fields: PropTypes.object
-}
-
-EditsTableRow.defaultProps = {
-  row: {},
-  fields: {}
 }
 
 export default EditsTableRow
