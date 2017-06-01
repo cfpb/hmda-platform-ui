e1cf1e5c62d4a5e9fa483f1fc142e10fcecade86
Andrew <awolfe76@gmail.com>
2017-06-01 13:08:13 -0400

    use usds alert, rework logic a bit

--- src/js/components/BannerDeadline.jsx
+++ src/js/components/BannerDeadline.jsx
@@ -1,24 +1,39 @@
 import React from 'react'
 import moment from 'moment'
 
-const BannerDeadline = (props) => {
-  // don't render if not in the filing period (1/1 to 3/1)
-  if(!moment(moment().format('YYYY-MM-DD')).isBetween(`${props.filingPeriod}-01-01`, `${props.filingPeriod}-03-01`, 'day', '[]')) {
-    return null
+export const withinAWeekOfDeadline = (today, filingPeriod) => {
+  if(!moment(today).isValid()) {
+    return false
   }
 
-  let alertClass = ''
-  // one month away
-  if(moment(moment().format('YYYY-MM-DD')).isBetween(`${props.filingPeriod}-02-01`, `${props.filingPeriod}-03-01`, 'day', '[]')) {
-    alertClass = 'warning'
+  if(moment(today).isBetween(`${filingPeriod}-02-21`, `${filingPeriod}-03-01`, 'day', '[]')) {
+    return true
   }
-  // one week away
-  if(moment(moment().format('YYYY-MM-DD')).isBetween(`${props.filingPeriod}-02-21`, `${props.filingPeriod}-03-01`, 'day', '[]')) {
-    alertClass = 'error'
+  return false
+}
+
+const BannerDeadline = (props) => {
+  const today = moment().format('YYYY-MM-DD')
+
+  // render if within the filing period (1/1/<filingPeriod> to 3/1/<filingPeriod>)
+  if(moment(today).isBetween(`${props.filingPeriod}-01-01`, `${props.filingPeriod}-03-01`, 'day', '[]')) {
+
+    let alertClass = 'usa-alert-info'
+    // warn if its within a week of the deadline
+    if(withinAWeekOfDeadline(today, props.filingPeriod)) {
+      alertClass = 'usa-alert-warning'
+    }
+
+    return (
+      <div className={`BannerDeadline usa-alert ${alertClass}`}>
+        <div className="usa-alert-body">
+          <p className="usa-alert-text">March 1st, {parseInt(props.filingPeriod, 10)+1} is the deadline to submit HMDA data for the {props.filingPeriod} filing period.</p>
+        </div>
+      </div>
+    )
   }
-  return (
-    <p className={`BannerDeadline ${alertClass}`}>March 1st, {parseInt(props.filingPeriod, 10)+1} is the deadline to submit HMDA data for the {props.filingPeriod} filing period.</p>
-  )
+
+  return null
 }
 
 BannerDeadline.propTypes = {
