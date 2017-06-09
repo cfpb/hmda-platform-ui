4cb744ce9e96684021e07e52c57d988d926ed7b4
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-06-09 01:12:42 -0700

    add legend and progress text

--- src/js/components/ValidationProgress.jsx
+++ src/js/components/ValidationProgress.jsx
@@ -2,28 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import * as STATUS from '../constants/statusCodes.js'
 
-/*
-Uploading
-Analyzing file format
-Verifying edits
-Edit verification complete
-*/
-
 const getFill = props => {
   let fillWidth = 0
+  let className = 'progressFill'
+
+  if(props.code === STATUS.PARSED_WITH_ERRORS) className += ' error'
+
   if(props.code <= STATUS.PARSING) fillWidth = props.percentUploaded/3
   if(props.code >= STATUS.PARSED) fillWidth = 50
   if(props.code === STATUS.PARSED_WITH_ERRORS ||
      props.code > STATUS.VALIDATING) fillWidth = 100
 
-  return <div className="progressFill" style={{width:fillWidth+'%'}}></div>
+  return <div className={className} style={{width:fillWidth+'%'}}></div>
 }
+
+const getText = props => {
+  let text = 'Uploading...'
+  if(props.code >= STATUS.PARSING) text = 'Analyzing file format...'
+  if(props.code === STATUS.PARSED_WITH_ERRORS) text = 'Invalid file format'
+  if(props.code === STATUS.VALIDATING) text = 'Verifying edits...'
+  if(props.code > STATUS.VALIDATING) text = 'Edit verification complete'
+
+  return (
+    <div className="progressText">
+      <span>{text}</span>
+      {getIndicator(props)}
+    </div>
+  )
+}
+
+const getIndicator = props => {
+  let className = 'progressIndicator'
+  if(props.code === STATUS.PARSED_WITH_ERRORS) className += ' error'
+  else if(props.code > STATUS.VALIDATING) className += ' complete'
+  else className += ' pulsing'
+  return <span className={className}></span>
+}
+
 const ValidationProgress = props => {
   return (
     <div className="ValidationProgress">
       <div className="progressTotal"></div>
       {getFill(props)}
-      {/*getText(props)*/}
+      {getText(props)}
     </div>
   )
 }
