10c54614374e6fd3b76f55b80d895bb65a910ded
Andrew <awolfe76@gmail.com>
2017-07-12 15:58:27 -0400

    update proptypes for the 'type' and to allow an additional html element

--- src/js/components/Alert.jsx
+++ src/js/components/Alert.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Alert = ({ type = 'info', heading, text }) => {
+const Alert = ({ type = 'info', heading, text, htmlElement }) => {
   if (!text) return null
 
   return (
@@ -9,15 +9,17 @@ const Alert = ({ type = 'info', heading, text }) => {
       <div className="usa-alert-body">
         {heading ? <h3 className="usa-alert-heading">{heading}</h3> : null}
         <p className="usa-alert-text">{text}</p>
+        {htmlElement ? htmlElement : null} {/* allow for additional html */}
       </div>
     </div>
   )
 }
 
 Alert.propTypes = {
-  type: PropTypes.string,
+  type: PropTypes.oneOf(['info', 'success', 'warning', 'error']),
   heading: PropTypes.string,
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  htmlElement: PropTypes.node
 }
 
 export default Alert
