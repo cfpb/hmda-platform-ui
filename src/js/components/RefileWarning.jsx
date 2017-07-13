fbfd90237567394868b7773d5268f3fea8e8820f
Andrew <awolfe76@gmail.com>
2017-07-13 11:34:20 -0400

    update refilewarning to use alerts plus some small refactoring to use the heading for the alerts

--- src/js/components/RefileWarning.jsx
+++ src/js/components/RefileWarning.jsx
@@ -2,48 +2,90 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router'
 import RefileButton from '../containers/RefileButton.jsx'
-import { PARSED_WITH_ERRORS, VALIDATED_WITH_ERRORS } from '../constants/statusCodes.js'
+import Alert from './Alert.jsx'
+import {
+  PARSED_WITH_ERRORS,
+  VALIDATED_WITH_ERRORS
+} from '../constants/statusCodes.js'
 
-export const getText = (props) => {
-  let textToRender = null
+export const getText = props => {
+  let text = null
 
-  if(props.syntacticalValidityEditsExist) {
-    textToRender = <div className="usa-alert-text"><p>Your file has <strong>syntactical and/or validity edits</strong>.</p><p>Please update your file and select the &quot;Upload a new file&quot; button.</p></div>
-  } else if(!props.qualityVerified && props.page === 'quality') {
-    textToRender = <div className="usa-alert-text"><p>Your file has <strong>quality edits</strong>.</p><p>You must verify the edits listed below and select the check box to confirm the accuracy of the data. If any of the data need to be corrected, please update your file and <RefileButton isLink={true} isLower={true} />.</p></div>
-  } else if(!props.macroVerified && props.page === 'macro') {
-    textToRender = <div className="usa-alert-text"><p>Your file has <strong>macro quality edits</strong>.</p><p>You must verify the edits listed below and select the check box to confirm the accuracy of the data. If any of the data need to be corrected, please update your file and <RefileButton isLink={true} isLower={true} />.</p></div>
+  if (props.syntacticalValidityEditsExist) {
+    text = [
+      'Please update your file and select the "Upload a new file" button.',
+      <RefileButton />
+    ]
+  } else if (!props.qualityVerified && props.page === 'quality') {
+    text = [
+      'You must verify the edits listed below and select the check box to confirm the accuracy of the data. If any of the data need to be corrected, please update your file and ',
+      <RefileButton isLink={true} isLower={true} />,
+      '.'
+    ]
+  } else if (!props.macroVerified && props.page === 'macro') {
+    text = [
+      'You must verify the edits listed below and select the check box to confirm the accuracy of the data. If any of the data need to be corrected, please update your file and ',
+      <RefileButton isLink={true} isLower={true} />,
+      '.'
+    ]
   }
 
-  if(props.code === PARSED_WITH_ERRORS) {
-    textToRender = <div className="usa-alert-text"><p>Your file has <strong>formatting errors</strong>.</p><p>Please update your file and click the &quot;Upload a new file&quot; button.</p></div>
+  if (props.code === PARSED_WITH_ERRORS) {
+    text = [
+      'Please update your file and click the "Upload a new file" button.',
+      <RefileButton />
+    ]
   }
 
-  return textToRender
+  return text
 }
 
-const RefileWarning = (props) => {
-  if (props.code > VALIDATED_WITH_ERRORS && props.code < PARSED_WITH_ERRORS) return null
-  if (props.page === 'syntacticalvalidity' && !props.syntacticalValidityEditsExist) return null
+export const getHeading = props => {
+  let heading = null
+
+  if (props.syntacticalValidityEditsExist) {
+    heading = 'Your file has syntactical and/or validity edits.'
+  } else if (!props.qualityVerified && props.page === 'quality') {
+    heading = 'Your file has quality edits.'
+  } else if (!props.macroVerified && props.page === 'macro') {
+    heading = 'Your file has macro quality edits.'
+  }
+
+  if (props.code === PARSED_WITH_ERRORS) {
+    heading = 'Your file has formatting errors.'
+  }
+
+  return heading
+}
+
+const RefileWarning = props => {
+  if (props.code > VALIDATED_WITH_ERRORS && props.code < PARSED_WITH_ERRORS)
+    return null
+  if (
+    props.page === 'syntacticalvalidity' &&
+    !props.syntacticalValidityEditsExist
+  )
+    return null
   if (props.page === 'quality' && props.qualityVerified) return null
   if (props.page === 'macro' && props.macroVerified) return null
   if (props.page === 'upload' && props.code !== PARSED_WITH_ERRORS) return null
   if (props.page === 'confirmation') return null
 
-  let alertClass = 'usa-alert-error'
-  if(!props.syntacticalValidityEditsExist && props.code !== PARSED_WITH_ERRORS) {
-    alertClass = 'usa-alert-warning'
+  let alertClass = 'error'
+  if (
+    !props.syntacticalValidityEditsExist &&
+    props.code !== PARSED_WITH_ERRORS
+  ) {
+    alertClass = 'warning'
   }
 
-  const text = getText(props)
-  const button = props.code === PARSED_WITH_ERRORS || props.syntacticalValidityEditsExist ? <RefileButton/> : null
-
   return (
-    <div className={`RefileWarning usa-alert ${alertClass}`}>
-      <div className="usa-alert-body">
-        {text}
-        {button}
-      </div>
+    <div className="RefileWarning">
+      <Alert
+        type={alertClass}
+        heading={getHeading(props)}
+        text={getText(props)}
+      />
     </div>
   )
 }
