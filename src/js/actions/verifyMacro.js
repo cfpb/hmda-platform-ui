fd8f7164538e67e135e8281df6aef6974e08ca99
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-08-02 13:41:13 -0700

    Make verifier check respond instantly to user interaction and show a loading spinner

--- src/js/actions/verifyMacro.js
+++ src/js/actions/verifyMacro.js
@@ -3,6 +3,7 @@ import * as types from '../constants'
 export default function verifyMacro(checked) {
   return {
     type: types.VERIFY_MACRO,
-    checked: checked
+    checked: checked,
+    isFetching: false
   }
 }
