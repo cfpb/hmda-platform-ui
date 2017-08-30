ac5606cbf7149d48abe91979fdd7784f115b839f
Andrew <awolfe76@gmail.com>
2017-08-30 16:40:28 -0400

    use the institution component

--- src/js/components/Institutions.jsx
+++ src/js/components/Institutions.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import InstitutionsHeader from './InstitutionsHeader'
-import InstitutionNameAndId from './InstitutionNameAndId'
-import InstitutionStatus from './InstitutionStatus'
-import InstitutionViewButton from './InstitutionViewButton'
-import InstitutionRefile from './InstitutionRefile'
-import InstitutionPreviousSubmissions from './InstitutionPreviousSubmissions'
 import LoadingIcon from './LoadingIcon.jsx'
 import ErrorWarning from './ErrorWarning.jsx'
+import Institution from './Institution.jsx'
 import { withinFilingPeriod } from '../utils/date.js'
 import Alert from './Alert.jsx'
 import * as STATUS from '../constants/statusCodes.js'
+
+export const renderHeaderOrAlert = filingPeriod => {
+  if (!filingPeriod) return null
+
+  if (withinFilingPeriod(filingPeriod)) {
+    return <InstitutionsHeader filingPeriod={this.props.filingPeriod} />
+  } else {
+    return (
+      <Alert type="warning" heading="The filing period is closed.">
+        <p>
+          The platform remains available outside of the filing period to upload,
+          test, and validate HMDA data.
+        </p>
+      </Alert>
+    )
+  }
+}
 
 export const getInstitutionFromFiling = (institutions, filing) => {
   for (let i = 0; i < institutions.length; i++) {
@@ -19,33 +31,16 @@ export const getInstitutionFromFiling = (institutions, filing) => {
   return null
 }
 
-export default class Institution extends Component {
+export default class Institutions extends Component {
   render() {
-    const institutions = this.props.institutions
-    const makeNewSubmission = this.props.makeNewSubmission
-
     return (
       <main id="main-content" className="usa-grid Institutions">
         {this.props.error ? <ErrorWarning error={this.props.error} /> : null}
         <div className="usa-width-one-whole">
-          {this.props.filingPeriod ? withinFilingPeriod(
-            this.props.filingPeriod
-          ) ? (
-            <InstitutionsHeader filingPeriod={this.props.filingPeriod} />
-          ) : (
-            <Alert type="warning" heading="The filing period is closed.">
-              <p>
-                The platform remains available outside of the filing period to
-                upload, test, and validate HMDA data.
-              </p>
-            </Alert>
-          ) : null}
-          {!this.props.filings.fetched ||
-          this.props.filings.isFetching ||
-          this.props.submission.isFetching ? (
+          {renderHeaderOrAlert(this.props.filingPeriod)}
+          {!this.props.filings.fetched || this.props.filings.isFetching || this.props.submission.isFetching ? (
             <LoadingIcon />
-          ) : this.props.filings.fetched &&
-          this.props.filings.filings.length === 0 ? (
+          ) : this.props.filings.fetched && this.props.filings.filings.length === 0 ? (
             <Alert type="error">
               <p>
                 There is a problem with your filing. Please contact{' '}
@@ -55,48 +50,25 @@ export default class Institution extends Component {
           ) : (
             this.props.filings.filings.map((filingObj, i) => {
               const filing = filingObj.filing
-              const submission =
-                this.props.submission.id &&
+              const submission = this.props.submission.id &&
                 this.props.submission.id.institutionId === filing.institutionId
-                  ? this.props.submission
-                  : filingObj.submissions[0]
-              const status = submission && submission.status
-              const institution = getInstitutionFromFiling(institutions, filing)
-
+                ? this.props.submission
+                : filingObj.submissions[0]
+              const institution = getInstitutionFromFiling(
+                this.props.institutions,
+                filing
+              )
               if (!institution) return
+
               return (
-                <div key={i} className="usa-grid-full">
-                  <section className="institution">
-                    <div className="current-status">
-                      <InstitutionNameAndId
-                        name={institution.name}
-                        id={institution.id}
-                      />
-
-                      <InstitutionStatus
-                        institutionId={institution.id}
-                        filing={filing}
-                        submission={submission}
-                        onDownloadClick={this.props.onDownloadClick}
-                      />
-
-                      <InstitutionViewButton
-                        status={status}
-                        institutionId={institution.id}
-                        filingPeriod={filing.period}
-                      />
-
-                      <InstitutionRefile status={status} filing={filing} />
-                    </div>
-
-                    <InstitutionPreviousSubmissions
-                      submissions={filingObj.submissions}
-                      institutionId={institution.id}
-                      filingPeriod={filing.period}
-                      onDownloadClick={this.props.onDownloadClick}
-                    />
-                  </section>
-                </div>
+                <Institution
+                  key={i}
+                  institution={institution}
+                  filing={filing}
+                  submission={submission}
+                  submissions={filingObj.submissions}
+                  onDownloadClick={this.props.onDownloadClick}
+                />
               )
             })
           )}
@@ -114,10 +86,9 @@ export default class Institution extends Component {
   }
 }
 
-Institution.propTypes = {
+Institutions.propTypes = {
   params: PropTypes.object,
   filings: PropTypes.object,
   institutions: PropTypes.array,
-  makeNewSubmission: PropTypes.func,
   onDownloadClick: PropTypes.func
 }
