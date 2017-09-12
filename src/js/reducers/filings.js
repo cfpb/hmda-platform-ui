8e59d9d2f309330a6b8283f6c3cbc38050e21db6
Andrew <awolfe76@gmail.com>
2017-09-12 16:08:48 -0400

    tests and cleanup

--- src/js/reducers/filings.js
+++ src/js/reducers/filings.js
@@ -1,4 +1,10 @@
-import { REQUEST_FILING,RECEIVE_FILING,CLEAR_FILINGS,RECEIVE_FILINGS, REFRESH_STATE } from '../constants'
+import {
+  REQUEST_FILING,
+  RECEIVE_FILING,
+  CLEAR_FILINGS,
+  RECEIVE_FILINGS,
+  REFRESH_STATE
+} from '../constants'
 import sortFilings from '../utils/sortFilings.js'
 
 const defaultFilings = {
@@ -14,31 +20,28 @@ const defaultFilings = {
  */
 export default (state = defaultFilings, action) => {
   switch (action.type) {
-  case REQUEST_FILING:
-    return {
-      ...state,
-      isFetching: true,
-      fetched: false
-    }
-  case RECEIVE_FILING:
-    console.log('RECEIVE_FILING')
-    console.log([...state.filings, action.filing])
-    console.log([...state.filings, action.filing].sort(sortFilings))
-    return {
-      ...state,
-      filings: [...state.filings, action.filing].sort(sortFilings)
-    }
-  case CLEAR_FILINGS:
-    return defaultFilings
-  case RECEIVE_FILINGS:
+    case REQUEST_FILING:
+      return {
+        ...state,
+        isFetching: true,
+        fetched: false
+      }
+    case RECEIVE_FILING:
+      return {
+        ...state,
+        filings: [...state.filings, action.filing].sort(sortFilings)
+      }
+    case CLEAR_FILINGS:
+      return defaultFilings
+    case RECEIVE_FILINGS:
       return {
         ...state,
         isFetching: false,
         fetched: true
       }
-  case REFRESH_STATE:
+    case REFRESH_STATE:
       return defaultFilings
-  default:
-    return state
+    default:
+      return state
   }
 }
