8156c925380ac864a65a4d07113127f68e5a3e8a
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-09-22 10:27:29 -0700

    add babel-env

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
+import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import 'babel-polyfill'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
