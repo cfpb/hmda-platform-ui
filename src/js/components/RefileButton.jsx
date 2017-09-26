8fd10069cad780e1abb5e672d8732dd923da4ed0
Andrew <awolfe76@gmail.com>
2017-09-26 11:23:19 -0400

    new function prop to set the institution when refile is clicked

--- src/js/components/RefileButton.jsx
+++ src/js/components/RefileButton.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const RefileButton = (props) => {
-  let refileStyle = 'RefileButton usa-button usa-button-secondary usa-text-small'
-  if(props.isLink) {
+const RefileButton = props => {
+  let refileStyle =
+    'RefileButton usa-button usa-button-secondary usa-text-small'
+  if (props.isLink) {
     refileStyle = ''
-    if(props.isLower) {
+    if (props.isLower) {
       refileStyle = 'text-lowercase'
     }
-    if(props.isSmall) {
+    if (props.isSmall) {
       refileStyle = `${refileStyle} usa-text-small`
     }
   }
 
-  return <a href="#" className={refileStyle}
-    onClick={(e)=>{
-      e.preventDefault()
-      props.showConfirmModal()
-    }}>Upload a new file</a>
+  return (
+    <a
+      href="#"
+      className={refileStyle}
+      onClick={e => {
+        e.preventDefault()
+        props.updateInstitution(props.institution)
+        props.showConfirmModal()
+      }}
+    >
+      Upload a new file
+    </a>
+  )
 }
 
 RefileButton.propTypes = {
+  institution: PropTypes.object,
+  updateInstitution: PropTypes.func.isRequired,
   showConfirmModal: PropTypes.func.isRequired,
   isLink: PropTypes.bool,
   isLower: PropTypes.bool,
