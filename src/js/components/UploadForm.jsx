c7bc8b30ffbc03a8a9e71ee7b43bc9e1f4925a90
Andrew <awolfe76@gmail.com>
2017-09-26 15:00:17 -0400

    minor clean up and formatting

--- src/js/components/UploadForm.jsx
+++ src/js/components/UploadForm.jsx
@@ -2,26 +2,22 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ValidationProgress from './ValidationProgress.jsx'
 import Dropzone from 'react-dropzone'
-import {
-  CREATED,
-  UPLOADING,
-  SIGNED
-} from '../constants/statusCodes.js'
+import { CREATED, UPLOADING, SIGNED } from '../constants/statusCodes.js'
 
-export const renderValidationProgress = (props) => {
-  if(props.code < UPLOADING && !props.uploading) return null
-  return <ValidationProgress file={props.file} code={props.code} id={props.id}/>
+export const renderValidationProgress = ({ code, uploading, file, id }) => {
+  if (code < UPLOADING && !uploading) return null
+  return <ValidationProgress file={file} code={code} id={id} />
 }
 
-export const renderErrors = (errors) => {
-  if(errors.length === 0) return null
+export const renderErrors = errors => {
+  if (errors.length === 0) return null
 
-  return(
+  return (
     <div className="usa-alert usa-alert-error" role="alert">
       <div className="usa-alert-body">
         <ul className="usa-alert-text">
           {errors.map((error, i) => {
-            return(<li key={i}>{error}</li>)
+            return <li key={i}>{error}</li>
           })}
         </ul>
       </div>
@@ -30,52 +26,72 @@ export const renderErrors = (errors) => {
 }
 
 export const getDropzoneText = ({ code, errors, filename }) => {
-  let howToMessage = 'To begin uploading a file, drag it into this box or click here.'
-  if(code >= CREATED) {
-    howToMessage = 'To begin uploading a new file, drag it into this box or click here.'
+  let howToMessage =
+    'To begin uploading a file, drag it into this box or click here.'
+  if (code >= CREATED) {
+    howToMessage =
+      'To begin uploading a new file, drag it into this box or click here.'
   }
   let message = <p>{howToMessage}</p>
 
-  if(code >= UPLOADING) {
+  if (code >= UPLOADING) {
     message = howToMessage
   }
 
-  if(code === SIGNED) {
-    message = <div>
-      <p>Your submission is complete.</p>
-      <p className="file-selected">{howToMessage}</p>
-    </div>
-  }
-
-  if(filename) {
-    message = <div>
-      <p><strong>{filename}</strong> selected.</p>
-      <p className="file-selected">{howToMessage}</p>
-    </div>
-
-    if(errors.length > 0) {
-      message = <div>
-        <p><strong>{filename}</strong> can not be uploaded.</p>
-        <p>{howToMessage}</p>
-      </div>
-    }
-
-    if(code >= UPLOADING) {
-      message = <div>
-        <p>Submission of <strong>{filename}</strong> is currently in progess.</p>
+  if (code === SIGNED) {
+    message = (
+      <div>
+        <p>Your submission is complete.</p>
         <p className="file-selected">{howToMessage}</p>
       </div>
-    }
+    )
+  }
 
-    if(code === SIGNED) {
-      message = <div>
-        <p>Your submission of <strong>{filename}</strong> is complete.</p>
+  if (filename) {
+    message = (
+      <div>
+        <p>
+          <strong>{filename}</strong> selected.
+        </p>
         <p className="file-selected">{howToMessage}</p>
       </div>
+    )
+
+    if (errors.length > 0) {
+      message = (
+        <div>
+          <p>
+            <strong>{filename}</strong> can not be uploaded.
+          </p>
+          <p>{howToMessage}</p>
+        </div>
+      )
+    }
+
+    if (code >= UPLOADING) {
+      message = (
+        <div>
+          <p>
+            Submission of <strong>{filename}</strong> is currently in progess.
+          </p>
+          <p className="file-selected">{howToMessage}</p>
+        </div>
+      )
+    }
+
+    if (code === SIGNED) {
+      message = (
+        <div>
+          <p>
+            Your submission of <strong>{filename}</strong> is complete.
+          </p>
+          <p className="file-selected">{howToMessage}</p>
+        </div>
+      )
     }
   }
 
-  return <button onClick={e=>e.preventDefault()}>{message}</button>
+  return <button onClick={e => e.preventDefault()}>{message}</button>
 }
 
 export default class Upload extends Component {
@@ -84,14 +100,9 @@ export default class Upload extends Component {
 
     // handle the onDrop to set the file and show confirmation modal
     this.onDrop = acceptedFiles => {
-      const {
-        code,
-        showConfirmModal,
-        setFile,
-        setNewFile
-      } = this.props
+      const { code, showConfirmModal, setFile, setNewFile } = this.props
 
-      if(code >= UPLOADING) {
+      if (code >= UPLOADING) {
         showConfirmModal()
         setNewFile(acceptedFiles)
       } else {
@@ -100,14 +111,11 @@ export default class Upload extends Component {
     }
   }
 
-  // keeps the info about the file after leaving /upload and coming back
   componentDidMount() {
-    if(this.props.code >= UPLOADING) this.props.pollSubmission()
+    if (this.props.code >= UPLOADING) this.props.pollSubmission()
   }
 
   render() {
-    const dropzoneText = getDropzoneText(this.props)
-
     return (
       <section className="UploadForm">
         {renderErrors(this.props.errors)}
@@ -116,7 +124,8 @@ export default class Upload extends Component {
             disablePreview={true}
             onDrop={this.onDrop}
             multiple={false}
-            className="dropzone">
+            className="dropzone"
+          >
             {dropzoneText}
           </Dropzone>
         </section>
