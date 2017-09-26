9213f9e2ceb218184bcadd7bed77e0f544a59e2f
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-09-26 16:30:54 -0700

    Complete switch to webpack

---
+++ webpack.renew.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: {
+    'main': './src/js/utils/silent_renew.js',
+  },
+  output: {
+    filename: 'silent_renew.js',
+    path: path.resolve(__dirname, 'dist/js'),
+  },
+  plugins: [
+    new UglifyJSPlugin({sourceMap: true}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': process.env.NODE_ENV || 'production'
+      }
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      include: [
+        path.resolve(__dirname, 'src/js'),
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules'),
+      ],
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['env', {
+              modules: false,
+              useBuiltIns: true,
+              targets: {
+                browsers: [
+                  'Chrome >= 60',
+                  'Safari >= 10.1',
+                  'iOS >= 10.3',
+                  'Firefox >= 54',
+                  'Edge >= 15',
+                ],
+              },
+            }],
+          ],
+        },
+      },
+    }],
+  },
+}
