33c572fae7f6c9ff5a84c5b3e8b2353e6c2d6507
Andrew <awolfe76@gmail.com>
2017-09-27 11:55:35 -0400

    improve messaging

--- src/js/components/BannerBeta.jsx
+++ src/js/components/BannerBeta.jsx
@@ -5,31 +5,15 @@ import Alert from './Alert.jsx'
 const BannerBeta = props => {
   return (
     <section className="BannerBeta usa-text-small">
-      <Alert type="warning" heading="This is a beta release.">
-        <div>
-          <p>
-            The beta HMDA Platform is a work in progress, intended to provide
-            HMDA filers with an understanding of the submission process, prior
-            to the filing period.
-          </p>
-          <p>
-            This is part of our work to improve the HMDA electronic reporting
-            process for financial institutions.
-          </p>
-          <p>
-            Contact us at{' '}
-            <a href="mailto:HMDAFeedback@cfpb.gov">HMDAFeedback@cfpb.gov</a> to
-            provide feedback on your experience with the HMDA Platform. Please
-            do not include personal information such as an account number,
-            personal address or Social Security number.
-          </p>
-          <p>
-            For more information about filing your HMDA data, please visit{' '}
-            <a href="https://www.consumerfinance.gov/hmda">
-              https://www.consumerfinance.gov/hmda/.
-            </a>
-          </p>
-        </div>
+      <Alert type="warning" heading="Help us improve this beta release.">
+        <p>
+          The beta HMDA platform is a work in progress, introducing HMDA filers
+          to the new submission process before the filing period. If you have
+          feedback on your experience, please send it to{' '}
+          <a href="mailto:HMDAFeedback@cfpb.gov">HMDAFeedback@cfpb.gov</a> (do
+          not include personal information such as an account number, address,
+          or Social Security number).
+        </p>
       </Alert>
     </section>
   )
