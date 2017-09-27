d6629c1d18f80295729a4f40a50a6ff99bf085f5
Andrew <awolfe76@gmail.com>
2017-09-27 09:25:56 -0400

    add message for open ended feedback

--- src/js/components/BannerBeta.jsx
+++ src/js/components/BannerBeta.jsx
@@ -2,16 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Alert from './Alert.jsx'
 
-const BannerBeta = (props) => {
+const BannerBeta = props => {
   return (
     <section className="BannerBeta usa-text-small">
-      <Alert
-        type="warning"
-        heading="This is a beta release">
+      <Alert type="warning" heading="This is a beta release.">
         <div>
-          <p>The beta HMDA Platform is a work in progress, intended to provide HMDA filers with an understanding of the submission process, prior to the filing period.</p>
-          <p>This is part of our work to improve the HMDA electronic reporting process for financial institutions.</p>
-          <p>For more information about filing your HMDA data, please visit <a href="https://www.consumerfinance.gov/hmda">https://www.consumerfinance.gov/hmda/.</a></p>
+          <p>
+            The beta HMDA Platform is a work in progress, intended to provide
+            HMDA filers with an understanding of the submission process, prior
+            to the filing period.
+          </p>
+          <p>
+            This is part of our work to improve the HMDA electronic reporting
+            process for financial institutions.
+          </p>
+          <p>
+            Contact us at{' '}
+            <a href="mailto:HMDAFeedback@cfpb.gov">HMDAFeedback@cfpb.gov</a> to
+            provide feedback on your experience with the HMDA Platform. Please
+            do not include personal information such as an account number,
+            personal address or Social Security number.
+          </p>
+          <p>
+            For more information about filing your HMDA data, please visit{' '}
+            <a href="https://www.consumerfinance.gov/hmda">
+              https://www.consumerfinance.gov/hmda/.
+            </a>
+          </p>
         </div>
       </Alert>
     </section>
