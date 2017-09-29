4b22e0cc83cf65883fb1a459daa70d575cb97ec4
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-09-28 21:57:39 -0700

    Split common code between renew and standard webpack builds

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+  },
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJSPlugin({sourceMap: true}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+      }
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      include: [
+        path.resolve(__dirname, 'src/js'),
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules'),
+      ],
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['env', {
+              modules: false,
+              useBuiltIns: true,
+              targets: {
+                browsers: [
+                  'Chrome >= 60',
+                  'Safari >= 10.1',
+                  'iOS >= 10.3',
+                  'Firefox >= 54',
+                  'Edge >= 15',
+                  'ie >= 11'
+                ],
+              },
+            }],
+          ],
+        },
+      },
+    }],
+  },
+}
--- webpack.renew.js
+++ webpack.renew.js
@@ -1,54 +1,11 @@
-const path = require('path')
-const webpack = require('webpack')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
 
-module.exports = {
+module.exports = merge(common, {
   entry: {
-    'main': './src/js/utils/silent_renew.js',
+    'main': './src/js/utils/silent_renew.js'
   },
   output: {
-    filename: 'silent_renew.js',
-    path: path.resolve(__dirname, 'dist/js'),
-  },
-  devtool: 'source-map',
-  plugins: [
-    new UglifyJSPlugin({sourceMap: true}),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
-      }
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin()
-  ],
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'src/js'),
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules'),
-      ],
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['env', {
-              modules: false,
-              useBuiltIns: true,
-              targets: {
-                browsers: [
-                  'Chrome >= 60',
-                  'Safari >= 10.1',
-                  'iOS >= 10.3',
-                  'Firefox >= 54',
-                  'Edge >= 15',
-                ],
-              },
-            }],
-          ],
-        },
-      },
-    }],
-  },
-}
+    filename: 'silent_renew.js'
+  }
+})
