bea66eabb207127c047749ffb1954e5161d0ea02
Andrew <awolfe76@gmail.com>
2017-10-17 16:17:38 -0400

    set NODE_ENV for each build

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,24 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const webpack = require('webpack')
 const WebpackShellPlugin = require('webpack-shell-plugin')
 
 module.exports = merge(common, {
   entry: {
-    'main': './src/js/index.js'
+    main: './src/js/index.js'
   },
   output: {
-    filename: 'app.min.js',
+    filename: 'app.min.js'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
+    }),
     new WebpackShellPlugin({
       onBuildStart: ['yarn run clearBackup'],
-      onBuildEnd:['yarn run env'],
+      onBuildEnd: ['yarn run env'],
       dev: false
     })
   ]
