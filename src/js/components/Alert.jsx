31840ec454a6e9fb1acc165732e0d507e596cd0c
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2017-12-05 13:42:26 -0800

    alertimage

--- src/js/components/Alert.jsx
+++ src/js/components/Alert.jsx
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types'
 
 const Alert = ({ type = 'info', heading, children }) => {
   if (!children) return null
+  const alertText = type === 'info' ? 'i' : type === 'warning' ? '!' : null
 
   return (
     <div className={`usa-alert usa-alert-${type}`}>
+      <div className="AlertImage">{alertText}</div>
       <div className="usa-alert-body">
-        {heading ? (
-          <h3 className="usa-alert-heading">
-            {heading}
-          </h3>
-        ) : null}
+        {heading ? <h3 className="usa-alert-heading">{heading}</h3> : null}
         {React.cloneElement(children, { className: 'usa-alert-text' })}
       </div>
     </div>
