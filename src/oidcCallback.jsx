ad761ea8a0b0ee93e9bb39710698abb16f4d9dca
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2018-02-12 22:14:26 -0800

    render an error on oidc-call errback

--- src/oidcCallback.jsx
+++ src/oidcCallback.jsx
@@ -3,7 +3,9 @@ import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
 import { set } from './api/AccessToken.js'
 import { restorePage, getUserManager } from './utils/redirect'
+import receiveError from './actions/receiveError.js'
 import Loading from './common/Loading.jsx'
+import Alert from './common/Alert.jsx'
 
 export class oidcCallback extends React.Component {
   successCallback(user) {
@@ -12,7 +14,31 @@ export class oidcCallback extends React.Component {
   }
 
   errorCallback(e) {
-    browserHistory.replace('/')
+    this.props.dispatch(receiveError(e))
+  }
+
+  renderError() {
+    return (
+      <Alert type="error" heading={this.props.error.message}>
+        <div>
+          We encountered the above problem logging you in. Please ensure:
+          <ul>
+            <li>this site is not being blocked or filtered by your firewall</li>
+            <li>
+              browser extensions that modify your web traffic are disabled
+            </li>
+            <li>the local time on your computer is set accurately</li>
+          </ul>
+          <p style={{ marginBottom: '0.5em' }}>
+            Please <a href={window.HMDA_ENV.APP_URL}>
+              return to the home page
+            </a>{' '}
+            and try again. If the problem persists, contact{' '}
+            <a href="mailto:hmdahelp@cfpb.gov">HMDA Help</a>.
+          </p>
+        </div>
+      </Alert>
+    )
   }
 
   componentWillMount() {
@@ -27,9 +53,13 @@ export class oidcCallback extends React.Component {
   }
 
   render() {
-    if (!this.props.location.hash) return null
+    if (this.props.error) return this.renderError()
     return <Loading className="floatingIcon" />
   }
 }
 
-export default connect()(oidcCallback)
+export function mapStateToProps(state) {
+  const { error } = state.app
+  return { error }
+}
+export default connect(mapStateToProps)(oidcCallback)
