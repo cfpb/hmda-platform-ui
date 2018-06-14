568ac8be301918ad40f30c62918b201f1d43e73b
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2018-06-14 06:39:39 -0700

    dev html and env task as part of js task

--- env.sh
+++ env.sh
@@ -9,26 +9,25 @@ get_baseurl () {
 KEYCLOAK_SERVER=$(get_baseurl ${KEYCLOAK_URL})
 HMDA_API_SERVER=$(get_baseurl ${HMDA_API})
 
-backups=($(ls ./dist/js/*.bak 2>/dev/null))
-if [ -f "${backups[1]}" ]; then
-  for i in "${backups[@]}"
+files=($(ls ./dist/js/*.js 2>/dev/null))
+  for i in "${files[@]}"
   do
-    name=$(echo "$i" | rev | cut -c 5- | rev)
-    sed \
-      -e "s@##HOMEPAGE_URL##@${HOMEPAGE_URL:-https://192.168.99.100}@"\
-      -e "s@##FILING_APP_URL##@${FILING_APP_URL:-https://192.168.99.100/filing/}@"\
-      -e "s@##HMDA_API##@${HMDA_API:-https://192.168.99.100:4443/hmda}@"\
-      -e "s@##KEYCLOAK_URL##@${KEYCLOAK_URL:-https://192.168.99.100:8443/auth/realms/hmda}@"\
-      "$i" > "$name"
+    if [ -f "$i.bak" ]; then
+      sed \
+        -e "s@##HOMEPAGE_URL##@${HOMEPAGE_URL:-https://192.168.99.100}@"\
+        -e "s@##FILING_APP_URL##@${FILING_APP_URL:-https://192.168.99.100/filing/}@"\
+        -e "s@##HMDA_API##@${HMDA_API:-https://192.168.99.100:4443/hmda}@"\
+        -e "s@##KEYCLOAK_URL##@${KEYCLOAK_URL:-https://192.168.99.100:8443/auth/realms/hmda}@"\
+        "$i.bak" > "$i"
+    else
+      sed -i.bak \
+        -e "s@##HOMEPAGE_URL##@${HOMEPAGE_URL:-https://192.168.99.100}@"\
+        -e "s@##FILING_APP_URL##@${FILING_APP_URL:-https://192.168.99.100/filing/}@"\
+        -e "s@##HMDA_API##@${HMDA_API:-https://192.168.99.100:4443/hmda}@"\
+        -e "s@##KEYCLOAK_URL##@${KEYCLOAK_URL:-https://192.168.99.100:8443/auth/realms/hmda}@"\
+        "$i"
+    fi
   done
-else
-  sed -i.bak \
-    -e "s@##HOMEPAGE_URL##@${HOMEPAGE_URL:-https://192.168.99.100}@"\
-    -e "s@##FILING_APP_URL##@${FILING_APP_URL:-https://192.168.99.100/filing/}@"\
-    -e "s@##HMDA_API##@${HMDA_API:-https://192.168.99.100:4443/hmda}@"\
-    -e "s@##KEYCLOAK_URL##@${KEYCLOAK_URL:-https://192.168.99.100:8443/auth/realms/hmda}@"\
-    ./dist/js/*.js
-fi
 
 if [ -f /etc/nginx/nginx.tmpl ]; then
   sed \
