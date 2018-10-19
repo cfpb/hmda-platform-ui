ae1aeba96b21ec58a62a1d5dc924575c26df9770
Andrew <awolfe76@gmail.com>
2018-10-19 14:37:00 -0400

    add in CRA polyfill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11' // For IE 11 support
+
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
