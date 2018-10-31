72fb3e4a145981c50670ae6d30d2d65205a6d51d
Andrew <awolfe76@gmail.com>
2018-10-31 09:01:05 -0400

    update filing period in API mocks

--- src/api/api.js
+++ src/api/api.js
@@ -83,13 +83,13 @@ export function getFiling(id, filing) {
           status: { code: 3, message: 'completed' },
           end: 1514741721031,
           start: 1514741668971,
-          period: '2017'
+          period: '2018'
         },
         submissions: [
           {
             fileName: 'Bank 0_400 LARS_S025 Ready.txt',
-            id: { lei: '0', period: '2017', sequenceNumber: 289 },
-            receipt: '0-2017-289-1540304400322',
+            id: { lei: '0', period: '2018', sequenceNumber: 289 },
+            receipt: '0-2018-289-1540304400322',
             status: {
               code: 10,
               message: 'Your submission has been accepted.',
@@ -101,7 +101,7 @@ export function getFiling(id, filing) {
           },
           {
             fileName: 'Bank 1_400 LAR_S025 ready.txt',
-            id: { lei: '0', period: '2017', sequenceNumber: 288 },
+            id: { lei: '0', period: '2018', sequenceNumber: 288 },
             receipt: '',
             status: {
               code: 8,
@@ -124,13 +124,13 @@ export function getFiling(id, filing) {
           status: { code: 3, message: 'completed' },
           end: 1515682213163,
           start: 1515682151087,
-          period: '2017'
+          period: '2018'
         },
         submissions: [
           {
             fileName: 'Bank 1_400 LAR_S025 ready.txt',
-            id: { lei: '1', period: '2017', sequenceNumber: 87 },
-            receipt: '1-2017-87-1536866772274',
+            id: { lei: '1', period: '2018', sequenceNumber: 87 },
+            receipt: '1-2018-87-1536866772274',
             status: {
               code: 10,
               message: 'Your submission has been accepted.',
@@ -142,7 +142,7 @@ export function getFiling(id, filing) {
           },
           {
             fileName: '777366.txt',
-            id: { lei: '1', period: '2017', sequenceNumber: 86 },
+            id: { lei: '1', period: '2018', sequenceNumber: 86 },
             receipt: '',
             status: {
               code: 5,
@@ -165,8 +165,8 @@ export function getLatestSubmission() {
   return new Promise(resolve => {
     resolve({
       fileName: 'Bank 0_400 LARS_S025 Ready.txt',
-      id: { lei: '0', period: '2017', sequenceNumber: 289 },
-      receipt: '0-2017-289-1540304400322',
+      id: { lei: '0', period: '2018', sequenceNumber: 289 },
+      receipt: '0-2018-289-1540304400322',
       status: {
         code: 9,
         message: 'Your data has edits that need to be reviewed.',
