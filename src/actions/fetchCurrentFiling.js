85967a789d8d37b0cbbe0c48767aebc305fa5b06
Andrew <awolfe76@gmail.com>
2018-11-15 20:53:06 -0500

    make the call for new filings sooner, when we see the filings array is empty

--- src/actions/fetchCurrentFiling.js
+++ src/actions/fetchCurrentFiling.js
@@ -1,15 +1,11 @@
 import fetchFiling from './fetchFiling.js'
-import fetchNewFiling from './fetchNewFiling.js'
 
-export default function fetchCurrentFiling(institution) {
+export default function fetchCurrentFiling(filings) {
   return (dispatch, getState) => {
     const period = getState().app.filingPeriod
-    const filing = institution.filings.filter(filing => {
+    const filing = filings.filter(filing => {
       return filing.period === period
     })[0]
-    if (filing) return dispatch(fetchFiling(filing))
-    return dispatch(
-      fetchNewFiling({ lei: institution.institution.lei, period: period })
-    )
+    return dispatch(fetchFiling(filing))
   }
 }
