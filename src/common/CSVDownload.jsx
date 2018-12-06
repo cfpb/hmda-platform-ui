0511ff8d4e2f01a20a288916c3f385dd91522aa2
Andrew <awolfe76@gmail.com>
2018-12-06 12:12:31 -0500

    the submission format is now id.lei (not id.institutionId)

--- src/common/CSVDownload.jsx
+++ src/common/CSVDownload.jsx
@@ -5,13 +5,13 @@ import './CSVDownload.css'
 
 const CSVDownload = props => {
   if (props.submission.id === null) return null
-  const { institutionId, period, sequenceNumber } = props.submission.id
+  const { lei, period, sequenceNumber } = props.submission.id
 
   return (
     <React.Fragment>
       <button
         className="CSVDownload"
-        onClick={props.onDownloadClick(institutionId, period, sequenceNumber)}
+        onClick={props.onDownloadClick(lei, period, sequenceNumber)}
         style={props.inline ? { display: 'inline', marginTop: 0 } : null}
       >
         {props.text || 'download the edit report'}
